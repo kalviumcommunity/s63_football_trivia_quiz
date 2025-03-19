c0748d092b59bdf98a5f46ada845bfdb8527d947
Vishwajeet Dubey <vishwajeet.dubey.s63@kalvium.community>
2025-03-19 10:07:54 +0530

    insert entity

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -8,6 +8,7 @@ const Header = () => {
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Login</Link></li>
+        <li><Link to="/add-question">Add Question</Link></li>
       </ul>
     </nav>
   );
--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./LandingPage";
 import LoginPage from "./LoginPage";
 import Quiz from "./Quiz";
+import QuestionForm from "./QuestionForm";
 import Header from "./Header";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/quiz" element={<Quiz />} />
+          <Route path="/add-question" element={<QuestionForm />} />
         </Routes>
       </div>
     </Router>
